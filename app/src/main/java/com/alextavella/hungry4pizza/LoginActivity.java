7338192f18b0a4fcebcfdb9c79f231c7eaaf45ab
alextavella <alextavella@outlook.com>
2017-05-29 22:46:10 -0300

    Login com TextInputLayout

--- app/src/main/java/com/alextavella/hungry4pizza/LoginActivity.java
+++ app/src/main/java/com/alextavella/hungry4pizza/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.alextavella.hungry4pizza;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextInputLayout tilLogin;
+    private TextInputLayout tilPassword;
     private EditText etLogin;
     private EditText etSenha;
 
@@ -21,19 +24,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //Recuperar os componentes da tela (xml)
-        etLogin = (EditText)findViewById(R.id.etLogin);
-        etSenha = (EditText)findViewById(R.id.etPassword);
+        tilLogin = (TextInputLayout)findViewById(R.id.tilLogin);
+        tilPassword = (TextInputLayout)findViewById(R.id.tilPassword);
+        /*etLogin = (EditText)findViewById(R.id.etLogin);
+        etSenha = (EditText)findViewById(R.id.etPassword);*/
 
         //Fill
-        etLogin.setText(USERNAME);
+        tilLogin.getEditText().setText(USERNAME);
     }
 
     // Clique do botão
     public void login(View view) {
 
         //Recuperar os valores digitados
-        String login = etLogin.getText().toString();
-        String senha = etSenha.getText().toString();
+        String login = tilLogin.getEditText().getText().toString();
+        String senha = tilPassword.getEditText().getText().toString();
 
         //Valida o usuário e senha
         if (login.equals(USERNAME) && senha.equals(PASSWORD)) {
